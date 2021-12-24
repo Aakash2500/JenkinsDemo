a1fd912ae65c053e29eb42cb23a356bd75205057
user <user@user-PC>
2021-12-24 13:44:24 +0530

    by me

--- 24.12.2021 Jenkins and Git/src/com/Jenkins_Git/Demo.java
+++ 24.12.2021 Jenkins and Git/src/com/Jenkins_Git/Demo.java
@@ -3,9 +3,17 @@ package com.Jenkins_Git;
 public class Demo {
 
 	
+	public void m1()
+	{
+		System.out.println("M1....Method");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hiii");
 		
 		System.out.println("Heloooo");
+		
+		
+	
 	}
 }
